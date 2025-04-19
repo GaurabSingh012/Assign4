95da9c2ceffad53d8c97251ae09b6edaa5e8ce96
Gaurab <gaurabkus781@gmail.com>
2025-04-19 19:35:55 +0530

    Implement createDeck method to generate standard 52-card deck

--- Deck.java
+++ Deck.java
@@ -1,7 +1,21 @@
+import java.util.ArrayList;
+
 class Deck {
     ArrayList<Card> deck;
 
     public Deck() {
         this.deck = new ArrayList<>();
     }
+
+    public void createDeck() {
+    String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+    String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+    for (String rank : ranks) {
+        for (String suit : suits) {
+            deck.add(new Card(rank, suit));
+        }
+    }
+    }
+
 }
