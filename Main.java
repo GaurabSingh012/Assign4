4620e23741f4783c2b2ac1dff129f11793ba3273
Gaurab <gaurabkus781@gmail.com>
2025-04-19 19:43:31 +0530

    Create the full deck in main and print a message

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 class Main {
     public static void main(String args[]) {
         Deck deck = new Deck(); 
+        
+        System.out.println("****" + "Creating Deck" + "****");
+        deck.createDeck();
     }
+
+    
+
 }
